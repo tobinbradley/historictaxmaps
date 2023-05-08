e980b82db4dd201608f5022836c81a406607e4a8
Tobin Bradley <tobin.bradley@gmail.com>
2023-05-08 13:31:04 -0400

    improve page sort

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { writable, readable, derived } from 'svelte/store'
 
 export const imgBaseUrl = readable('//maps.co.mecklenburg.nc.us/taxmaps/')
-export let imgUrl = writable("1970-00101.webp")
+export let imgUrl = writable("1970-001Index.webp")
 export let imgTitle = writable('')
 export const book = writable('001')
 export const books = writable([])
@@ -20,7 +20,7 @@ fetch('https://api.mcmap.org/v1/query/historic_tax_maps?columns=distinct(book)%2
 
 // fetch records
 book.subscribe(val => {
-  fetch(`https://api.mcmap.org/v1/query/historic_tax_maps?columns=year%2C%20book%2C%20page%2C%20file&filter=book%3D'${val}'&sort=year%2C%20book%2C%20page`)
+  fetch(`https://api.mcmap.org/v1/query/historic_tax_maps?columns=year%2C%20book%2C%20page%2C%20file&filter=book%3D'${val}'&sort=year%2C%20book%2C%20CASE%20WHEN%20page%20~%20'%5E%5Ba-zA-Z%5D'%20THEN%201%20WHEN%20page%20~%20'%5E%5B0-9%5D'%20THEN%202%20END`)
     .then(response => response.json())
     .then(json => {
       records.set(json)
