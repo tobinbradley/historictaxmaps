3105351fb5d1f64b66a6a71fb166e99eb6423742
tobinbradley <tobin.bradley@gmail.com>
2023-05-09 10:24:11 -0400

    state and hash navigation

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-import { writable, readable, derived } from 'svelte/store'
+import { writable, readable } from 'svelte/store'
 
 export const imgBaseUrl = readable('//maps.co.mecklenburg.nc.us/taxmaps/')
 export let imgUrl = writable("1970-001Index.webp")
@@ -7,6 +7,26 @@ export const book = writable('001')
 export const books = writable([])
 export const records = writable([])
 
+// Handle nav events
+let browserNav = false
+
+function readHash() {
+  const arg = window.location.hash.replace('#', '')
+  if (arg.length > 1) {
+    imgUrl.set(arg)
+    book.set(arg.substring(5, 8))
+  }
+}
+readHash()
+
+imgUrl.subscribe(val => {
+  if (!browserNav) history.pushState( {} , '', `#${val}` )
+  browserNav = false
+})
+window.addEventListener('popstate', (event) => {
+  browserNav = true
+  readHash()
+})
 
 // fetch books
 fetch('https://api.mcmap.org/v1/query/historic_tax_maps?columns=distinct(book)%20as%20book&sort=book')
